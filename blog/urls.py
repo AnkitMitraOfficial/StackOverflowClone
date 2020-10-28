2cb005eccd758fb3a2b0ee338053a40ff80dd499
Ankit <ankitmitra486@gmail.com>
2020-10-28 12:00:08 +0530

    Updating the blog feature!

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from django.conf import settings
 from . import views
+
 
 urlpatterns = [
     path('',views.all_blog,name='blog'),
+    # path('<int:blog_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def all_blog(request):
     blogs = Blog.objects.order_by('-date')[:5]
     return render (request,'blog/all_blog.html',{'blogs':blogs})
+
+# def detail(request, blog_id):
+#     blog = get_object_or_404(Blog, pk=blog_id)
+#     return render(request, 'blog/detail.html',{'blog':blog})
