6f476d3628d461e360caad12724e8a2040f836fc
Thomas Guettler <guettliml@thomas-guettler.de>
2020-12-02 08:41:10 +0100

    unified name: StackOverflowClone. See #17

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="StackOverflowClone",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AnkitMItraOfficial/StackOverflow-Clone",
+    url="https://github.com/AnkitMItraOfficial/StackOverflowClone",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
